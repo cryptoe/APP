77d7317098c8d7b8791aef2839e6cc228694d08a
amit1618 <amit.stiffy90@gmail.com>
2014-11-22 00:03:41 +0530

    Yosi comments implemented

--- js/home.js
+++ js/home.js
@@ -1,30 +1,122 @@
+var jsonData;
+function clicked(val)
+{
+    var ni = document.getElementById("btn");
+    ni.innerHTML = val;
+    $( "#positionWindow" ).popup( "close" );
+//     document.getElementById("positionWindow").style.display = 'none';
+}
+$( document ).ready(function() {  
+
+    $("#region").change(function () {
+        
+        alert("sssss");
+       $.mobile.loading( "show", {
+            text: "",
+            textVisible: false,
+            theme: "z",
+            html: ""
+        });
+          filterData($("#title-filter-menu").val(),$("#title-filter-menu1").val());
+    }); 
+
+   $("#title-filter-menu1").change(function () {
+       $.mobile.loading( "show", {
+            text: "",
+            textVisible: false,
+            theme: "z",
+            html: ""
+        });
+       filterData($("#title-filter-menu").val(),$("#title-filter-menu1").val());
+    }); 
+
+});
+function filterData(regionData,priceData)
+{
+    //filter by region
+        divElement="";
+        tempRegionJsonData = [];
+        tempPriceJsonData = [];
+        counter=0;
+    
+        if(regionData == -1 || regionData == 0)
+        {
+            tempRegionJsonData = jsonData;
+        }
+        else
+        {
+             for (var i = 0; i < jsonData.length; i++) {
+                    var region = jsonData[i]['region'];
+                    if(regionData == region)
+                    {
+                         tempRegionJsonData[counter] = jsonData[i];
+                        counter = counter + 1;
+                    }
+             }
+        }
+
+        //Filter by Price
+        if(priceData == -1 || priceData == 0)
+        {
+            tempPriceJsonData = tempRegionJsonData;
+        }
+        else
+        {
+            counter = 0;
+             for (var i = 0; i < tempRegionJsonData.length; i++) {
+                    var price = tempRegionJsonData[i]['price'];
+                    if(priceData >= price)
+                    {
+                         tempPriceJsonData[counter] = tempRegionJsonData[i];
+                        counter = counter + 1;
+                    }
+             }
+        }
+        //Add filtered Data to div
+         for (var i = 0; i < tempPriceJsonData.length; i++) {
+                    var name = tempPriceJsonData[i]['name'];
+                    var logo = tempPriceJsonData[i]['logo'];
+                    var price = tempPriceJsonData[i]['price'];
+                    var region = tempPriceJsonData[i]['region'];
+                     divElement = divElement + "<a href='#about'><img src=\'" + logo + "\' alt=\'"+name+"\' style=\"width:100px;height:100px;\"></a>";
+                   // divElement = divElement + "<a href='#about'><div style=\"width:100px;height:100px;border:1px solid black;background:url('"+logo+"');\"></div></a>";
+             }
+    addElement('content', divElement);
+}
 function addElement(divId, divString) {
     var ni = document.getElementById(divId);
     ni.innerHTML = divString;
+     $.mobile.loading("hide");
 }
 
 function setClient(data, t, p) {
     console.log(data);
     var divElement = "";
-
+     $.mobile.loading( "show", {
+            text: "",
+            textVisible: false,
+            theme: "z",
+            html: ""
+        });
     for (var i = 0; i < data.length; i++) {
+        jsonData = data;
         var name = data[i]['name'];
         var logo = data[i]['logo'];
         var price = data[i]['price'];
         var region = data[i]['region'];
-        console.log(
-
-            data[i]['name'] + " logo :" + data[i]['logo'] + " price :" + data[i]['price'] + "region " + data[i]['region']);
+      //  console.log(
+        //    data[i]['name'] + " logo :" + data[i]['logo'] + " price :" + data[i]['price'] + " region " + data[i]['region']);
         //divElement = divElement + "<div class=\'col-xs-3 tile\'><img src=\'" + logo + "\'/><p>" + name + "</p></div>";
-        divElement = divElement + "<img src=\'" + logo + "\'/>";
+      // divElement = divElement + "<div class=\"content_div\"><a href='#about'><img src=\'" + logo + "\' alt=\'"+name+"\' style=\"width:100px;height:100px;\"><p style=\"margin-top:0%\">\'"+name+"\'</p></a></div>";
+     //   divElement = divElement + "<a href='#about'><div style=\"width:100px;height:100px;border:1px solid black;background:url('"+logo+"');\"></div></a>";
+         divElement = divElement + " <li class=\"iconLeft\" data-icon=\"arrow-l\"><a href='#about'><div style=\"display: inline-block; float: right;\"><img src=\'" + logo + "\' style=\"width:100px;height:100px\"/></div><div style=\"display: inline-block; float: right; padding-right: 10px;\"><h1>"+name+"</h1><h2 align=\"right\">"+price+"</h2></div></a></li>"
     }
     console.log(divElement);
-    addElement('content', divElement);
+    addElement('photographersList', divElement);
     // outputs 'Foo'
 }
 
 //For Region and Price Select menu
-
 ( function( $ ) {
 function pageIsSelectmenuDialog( page ) {
     var isDialog = false,
@@ -101,4 +193,9 @@ $.mobile.document
         // Put the form back in the popup. It goes ahead of the listview.
         listview.before( form );
     });
+
+   
+
 })( jQuery );
+
+    
